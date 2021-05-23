2803f06d62102304183c28e6e42b4086e0b099f2
erikhoj <erikhoj.p@gmail.com>
2021-05-23 08:56:01 +0200

    Now parsing some of the first HTTP Request.
    Co-authored-by: Mathias Sjørslev Søholm <mathias.sjoerslev.soeholm@lego.com>

--- src/websocket.rs
+++ src/websocket.rs
@@ -1,14 +1,15 @@
 use crate::protocol::Protocol;
-use std::io::prelude::*;
+use std::{io::prelude::*, ops::Index};
 use std::net::TcpStream;
 
 pub struct WebSocket<'a> {
-  stream: &'a dyn WebSocketStream,
+  stream: &'a mut dyn WebSocketStream,
   protocol: Protocol,
 }
 
 pub trait WebSocketStream {
   fn read(&mut self, buf: &mut [u8]) -> Result<usize, std::io::Error>;
+  fn write(&mut self, buf: &[u8]) -> Result<usize, std::io::Error>;
 }
 
 struct TcpWebSocketStream<'a>(&'a mut TcpStream);
@@ -17,18 +18,54 @@ impl WebSocketStream for TcpWebSocketStream<'_> {
   fn read(&mut self, buf: &mut [u8]) -> Result<usize, std::io::Error> {
     TcpStream::read(self.0, buf)
   }
+
+  fn write(&mut self, buf: &[u8]) -> Result<usize, std::io::Error> {
+    TcpStream::write(self.0, buf)
+  }
 }
 
 impl<'a> WebSocket<'a> {
-  pub fn new(stream: &'a WebSocketStream) -> WebSocket<'a> {
+  pub fn new(stream: &'a mut dyn WebSocketStream) -> WebSocket<'a> {
     WebSocket {
       stream: stream,
       protocol: Protocol::new(),
     }
   }
+
+  pub fn open(&mut self) {
+    let mut last_was_r = false;
+    let mut saw_crlf = false;
+    let mut message_end_index = None;
+
+    while message_end_index == None {
+      let bytes = read_from_stream(self.stream);
+      
+      for (index, &b) in bytes.iter().enumerate() {
+        if last_was_r && b == b"\n"[0] {
+          if saw_crlf {
+            message_end_index = Some(index);
+            break;
+          }
+  
+          saw_crlf = true;
+          last_was_r = false;
+          continue;
+        }
+  
+        last_was_r = b == b"\r"[0];
+  
+        if !last_was_r {
+          saw_crlf = false;
+        }
+      }
+    }
+
+    // TODO - Make sure that we support HTTP Requests that are longer than 512 bytes?
+    println!("{:?}", message_end_index);
+  }
 }
 
-fn read_from_stream(mut stream: TcpStream) -> Vec<u8> {
+fn read_from_stream(stream: &mut dyn WebSocketStream) -> Vec<u8> {
   let mut buffer = vec![0; 512];
   let result = stream.read(buffer.as_mut_slice()).unwrap();
 
--- tests/websocket_test.rs
+++ tests/websocket_test.rs
@@ -1,9 +1,11 @@
 use rust_websocket::{WebSocket,WebSocketStream};
 use std::cmp;
 
+#[derive(Debug)]
 struct FakeStream {
   message: Vec<u8>,
   cursor: usize,
+  written: Option<Vec<u8>>,
 }
 
 impl FakeStream {
@@ -11,7 +13,8 @@ impl FakeStream {
   fn new(message: Vec<u8>) -> FakeStream{
     FakeStream {
       message,
-      cursor: 0
+      cursor: 0,
+      written: None,
     }
   }
 }
@@ -20,14 +23,31 @@ impl WebSocketStream for FakeStream {
   fn read(&mut self, buf: &mut [u8]) -> Result<usize, std::io::Error> {
     let size = cmp::min(self.message.len() - self.cursor, buf.len());
 
-    buf.copy_from_slice(&self.message[self.cursor..(self.cursor + size)]);
+    let data = &self.message[self.cursor..(self.cursor + size)];
+    
+    for (place, data) in buf.iter_mut().zip(data.iter()) {
+      *place = *data;
+    }
+
     self.cursor += size;
     Ok(size)
+  }
+
+  fn write(&mut self, buf: &[u8]) -> Result<usize, std::io::Error> {
+    self.written = Some(buf.to_vec());
+    Ok(buf.len())
   }
 }
 
 #[test]
 fn it_works() {
-  let fake_stream = FakeStream::new(vec![0]);
-  let ws = WebSocket::new(&fake_stream);
-}
+  let handshake_message = b"GET / HTTP/1.1\r\nHost: example.com:8000\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version: 13\r\n\r\n";
+
+  let mut fake_stream = FakeStream::new(handshake_message.to_vec());
+  let mut ws = WebSocket::new(&mut fake_stream);
+
+  ws.open();
+
+  let handshake_response = b"HTTP/1.1 101 Switching Protocols\nUpgrade: websocket\nConnection: Upgrade\nSec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=";
+  assert_eq!(fake_stream.written.unwrap(), handshake_response);
+} 
